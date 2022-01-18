0578e0d3408703fd1a757a65bb4178105793d193
Network Dude <networkdude2k@gmail.com>
2022-01-17 20:55:17 -0600

    Committed

---
+++ functions.py
@@ -0,0 +1,13 @@
+# Learning to define and use functions
+
+# Define the function
+def thing() :
+    print('Hello')
+    print('Fun')
+
+# Invoke, or call, the function
+thing()
+
+print('Zip')
+
+thing()
