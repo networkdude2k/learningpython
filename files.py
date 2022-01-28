fcbf49895d61ab2d425822b8eab7c91c08290007
Nate Walker <networkdude2k@gmail.com>
2022-01-27 22:41:00 -0600

    Committed

--- files.py
+++ files.py
@@ -1,6 +1,15 @@
-fhand = open('mbox.txt')
+# fhand = open('mbox.txt')
 
+fname = input('Enter the file name:  ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    quit()
+
+count = 0
 for line in fhand:
-    line = line.rstrip()
-    if line.startswith('From:'):
-        print(line)
+#   line = line.rstrip()
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname + '.')
